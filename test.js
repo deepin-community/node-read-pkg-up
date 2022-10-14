a9b4cdb87104fad95f7457fc32d02434f15c091d
Deepin Developer <deepin-dev@deepin.org>
2022-10-14 15:03:32 +0800

    feat: Init commit
    init node-read-pkg-up
    
    Log:

---
+++ test.js
@@ -0,0 +1,22 @@
+import path from 'path';
+import test from 'ava';
+import readPackageUp from '.';
+
+const cwd = 'fixture';
+const packagePath = path.resolve('.', 'package.json');
+
+test('async', async t => {
+	const result = await readPackageUp({cwd});
+	t.is(result.packageJson.name, 'read-pkg-up');
+	t.is(result.path, packagePath);
+
+	t.is(await readPackageUp({cwd: '/'}), undefined);
+});
+
+test('sync', t => {
+	const result = readPackageUp.sync({cwd});
+	t.is(result.packageJson.name, 'read-pkg-up');
+	t.is(result.path, packagePath);
+
+	t.is(readPackageUp.sync({cwd: '/'}), undefined);
+});
